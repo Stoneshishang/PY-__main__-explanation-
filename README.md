f0a43cdca2373fdc170cfe2b699a8eead996d0ac
Shi <stoneshishang@gmail.com>
2021-04-01 13:30:22 -0400

    finished decorator sample, add *args and **kwargs explanation

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
-This is the explanation of **main** in python, following the youtube https://www.youtube.com/watch?v=sugvnHA7ElY&ab_channel=CoreySchafer
+This is the explanation of **main** in python, following the youtube. https://www.youtube.com/watch?v=sugvnHA7ElY&ab_channel=CoreySchafer
 
-Add the sample code of PY decorator explanation.https://www.datacamp.com/community/tutorials/decorators-python
+Add the sample code of PY decorator explanation. https://www.datacamp.com/community/tutorials/decorators-python
+
+Add the usage of \*args and \*\*kwargs in pythong. from https://book.pythontips.com/en/latest/args_and_kwargs.html
---
+++ argsAndkwargs.py
@@ -0,0 +1,40 @@
+# the usage of *args, which is send a non-keyworded variable length argument list to the function.
+
+def test_var_args(f_arg, *argv):
+    print("first normal age:", f_arg)
+    for arg in argv:
+        print("another arg through *argv:", arg)
+
+
+test_var_args('java', 'python', 'C#', 'php')
+
+print('-------------------------------------------')
+
+# the usage of **kwargs, which passes keywored variuable length of arguments to a function.it is used
+# if I want to handle named arguments in a function.
+
+
+def greet_me(**kwargs):
+    for key, value in kwargs.items():
+        print("{0}  {1}".format(key, value))
+
+
+greet_me(name="java")
+
+print('-------------------------------------------')
+# Using *args and **kwargs to call a function
+
+
+def test_args_kwargs(arg1, arg2, arg3):
+    print("arg1:", arg1)
+    print("arg2:", arg2)
+    print("arg3:", arg3)
+
+
+args = ("two", "3", "5")
+test_args_kwargs(*args)
+
+print('-------------------------------------------')
+
+kwargs = {"arg3": 3, "arg2": 'two', "arg1": 5}
+test_args_kwargs(**kwargs)
