de3a482f67e409b81f9bbedff37a7b3e368a0a88
Shi <stoneshishang@gmail.com>
2021-04-09 15:37:18 -0400

    first class function and example added

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 This is the explanation of **main** in python, following the youtube. https://www.youtube.com/watch?v=sugvnHA7ElY&ab_channel=CoreySchafer
 
+First-Class Functions https://youtu.be/kr0mpwqttM0
+
 Add the sample code of PY decorator explanation. https://www.datacamp.com/community/tutorials/decorators-python
+Add Corey's Decorator explanation. https://www.youtube.com/watch?v=FsAPt_9Bf3U&ab_channel=CoreySchafer
 
 Add the usage of \*args and \*\*kwargs in pythong. from https://book.pythontips.com/en/latest/args_and_kwargs.html
---
+++ first_class_functions.py
@@ -0,0 +1,44 @@
+def square(x):
+    return x * x
+
+
+def cube(x):
+    return x * x * x
+
+
+def my_map(func, arg_list):
+    result = []
+    for i in arg_list:
+        result.append(func(i))
+    return result
+
+ # square is the first class function, without ()
+squares = my_map(cube, [1, 2, 3, 4, 5])
+
+print(squares)
+
+
+def logger(msg):
+    def log_message():
+        print("Log:", msg)
+
+    return log_message
+
+
+log_hi = logger('Hi!')
+log_hi()
+
+# example:
+
+
+def html_tag(tag):
+    def wrap_text(msg):
+        print('<{0}>{1}</{0}>'.format(tag, msg))
+    return wrap_text
+
+
+print_h1 = html_tag('h1')
+print_h1('Test Headline!')
+
+print_p = html_tag('p')
+print_p('Test Paragraph!')
